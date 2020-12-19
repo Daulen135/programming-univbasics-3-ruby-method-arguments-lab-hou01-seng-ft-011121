f20e39c6c65d424de7093382cb3ab6dee731a6ef
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-19 06:16:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ introduction ("Maria")
 
 
 def introduction_with_language (name, language = "computer")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language} programmer."
 end
 introduction ("Maria","Ruby")
 
