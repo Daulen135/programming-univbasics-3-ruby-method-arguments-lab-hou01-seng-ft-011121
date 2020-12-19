753fa949836c6b379bf4bc90b32c6bddc54c28d5
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-19 06:30:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ end
 introduction_with_language("Maria","Ruby")
 
 def introduction_with_language_optional (name,language=Ruby)
-  puts
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
